6df072f2066ee488b9b0314900fb31b5031b95c3
Rúben Campos <2078820@student.uma.pt>
2024-12-18 11:32:43 +0000

    feat: Added Random Donate Button to searchPage

--- src/app/SearchPage/CampaignsList.tsx
+++ src/app/SearchPage/CampaignsList.tsx
@@ -17,11 +17,23 @@ import Carousel from "../components/carousell";
 
 const CampaignsList :React.FC<{campaigns:Campaign[]}> = ({campaigns}) =>{
     //console.log(campaigns);
+    const campaigns_Ids = campaigns.map((c) => c.id);
+
+    // Generate a random index
+    const randomIndex = Math.floor(Math.random() * campaigns_Ids.length);
+  
+    // Use the random index for any logic as needed
+    const randomCampaignId = campaigns_Ids[randomIndex];
 
     return(
       <div className={styles.container}>
         <div><Carousel/></div>
               <div className={styles.campaignContainer}>
+                <div className={styles.buttoncontainer}>
+                  <a href={`/campaigns/view/${randomCampaignId}`}>
+                <button className={styles.donateNowButton}> Random Donate</button>
+                  </a>
+                </div>
                 <h2 className={styles.heading}>Other Campaigns</h2>
                 {/* List of campaigns rendered dynamically from defaultCampaigns */}
                 <div className={styles.campaignList}>
