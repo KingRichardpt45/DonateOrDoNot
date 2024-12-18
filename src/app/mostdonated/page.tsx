bf96e18e132a788baa9a80f1f4650ed18aed866e
Pedro Ferreira <pedroferreira0620@gmail.com>
2024-12-18 21:18:08 +0000

    feat: most frequent and most donated

--- src/app/mostdonated/page.tsx
+++ src/app/mostdonated/page.tsx
@@ -2,35 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Donor } from "@/models/Donor";
 import { DonorManager } from "@/core/managers/DonorManager";
 import { MainLayout } from "../components/coreComponents/mainLayout";
-import TopDonorsList from "../components/topDonors/TopDonorsList";
-import styles from "./rankingPage.module.css";
-import TopDonors from '../components/topDonors/topDonors';
+import styles from "./mostDonated.module.css";
+import TopDonatedList from '../components/topDonated/TopDonatedList';
+import TopDonated from '../components/topDonated/topDonated';
 
 const donorManager = new DonorManager();
 
-export default async function RankingPage() {
+export default async function mostDonated() {
   const top10 = 10;
   const top5 = 5;
 
   // TOP 10 list
-  const top10donorListResult = await donorManager.getTopTotalValueDonors(0,top10);
+  const top10donorListResult = await donorManager.getTopTotalDonationsValueDonors(0,top10);
   const top10donorList = top10donorListResult.value;
   //console.log(donorList);
   
   //TOP 5 LIST
-  const top5donorListResult = await donorManager.getTopTotalValueDonors(0,top5);
+  const top5donorListResult = await donorManager.getTopTotalDonationsValueDonors(0,top5);
   const top5donorList:Donor[] = top5donorListResult.value!;
 
   return (
     <MainLayout passUser={null}>
       <div className={styles.page}>
         <main className={styles.main}>
-          <TopDonors
+          <TopDonated
             podiumDonors={top5donorList}
           />
           <div className={styles.topDonorsList}>
             {top10donorList && top10donorList.map((donor, index) => (
-              <TopDonorsList
+              <TopDonatedList
               key={index}
               donor={donor}
               index={index + 1} 
