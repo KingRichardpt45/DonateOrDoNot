736a81ddf3df15cf38fc5781be240040e2d99eeb
Rúben Aguiar <2076820@student.uma.pt>
2024-12-18 23:17:08 +0000

    small improvements

--- src/app/components/storeItem/shopItem.tsx
+++ src/app/components/storeItem/shopItem.tsx
@@ -1,46 +1,65 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './shopItem.module.css';
 import {StoreItem} from '@/models/StoreItem';
 import {File as ModelFile} from "@/models/File";
+import {ActionDisplay} from "@/app/components/actionsNotifications/actionDisplay/ActionDisplay";
+import {ActionResultNotificationError} from "@/app/components/actionsNotifications/ActionResultNotificationError";
+import {IActionResultNotification} from "@/app/components/actionsNotifications/IActionResultNotification";
+import {ActionResultNotificationSuccess} from "@/app/components/actionsNotifications/ActionResultNotificationSuccess";
 
-export const ShopItem: React.FC<{item:StoreItem, userID:number}> = ({item, userID})=>{
+export const ShopItem: React.FC<{ item: StoreItem, userID: number }> = ({item, userID}) => {
 
-  const getItem = async () => {
-    try {
-      const formData = new FormData();
-      formData.append("store_item_id", item.id!.toString() );
-      formData.append("donor_id", userID.toString());
+    const [actionResults, setActionResults] = useState<IActionResultNotification[]>([]);
 
-      // Send donation data to the API
-      const response = await fetch("/api/store/",  { method: "POST", body: formData });
+    const getItem = async () => {
+        try {
+            const formData = new FormData();
+            formData.append("store_item_id", item.id!.toString());
+            formData.append("donor_id", userID.toString());
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Purchase Succesfull");
-      } else {
-        const error = await response.json();
-        console.log("Purchase Failed");
-      }
-    } catch (error) {
-      console.log("An error occurred. Please try again.");
-    } finally {
-      console.log("An error occurred. Please try again.");
-    }
-  };
+            // Send donation data to the API
+            const response = await fetch("/api/store/", {method: "POST", body: formData});
+            setActionResults([]);
 
-  return (
-    <div className={styles.itemContainer}>
-        <div className={styles.itemImage}>
-            <img src={`/documents/${item.image_id}_${(item.image.value as ModelFile).original_name}`} alt={`${(item.image.value as ModelFile).original_name}`} width={100} height={100} />
+            if (response.ok) {
+                const actionsNotificationsError = [new ActionResultNotificationSuccess("Product added to your profile!", 2000)];
+                setActionResults(actionsNotificationsError);
+            } else {
+                const actionsNotificationsError = [new ActionResultNotificationError("An error occured.", [], 2000)];
+                setActionResults(actionsNotificationsError);
+            }
+        } catch (error) {
+            const actionsNotificationsError = [new ActionResultNotificationError("An error occured.", [], 2000)];
+            setActionResults(actionsNotificationsError);
+        } finally {
+            setTimeout(() => {
+                setActionResults([]);
+            }, 2000);
+        }
+    };
+
+    return (
+        <div>
+            {
+                actionResults.length > 0 &&
+                (
+                    <ActionDisplay actions={actionResults}/>
+                )
+            }
+            <div className={styles.itemContainer}>
+                <div className={styles.itemImage}>
+                    <img src={`/documents/${item.image_id}_${(item.image.value as ModelFile).original_name}`}
+                         alt={`${(item.image.value as ModelFile).original_name}`} width={100} height={100}/>
+                </div>
+                <div className={styles.itemDescription}>
+                    <p>{item.name}</p>
+                    <p>{item.cost}
+                        <img src="/images/donacoin.png" alt="Dona Coin" width={15} height={15}/>
+                    </p>
+                    <button onClick={getItem} className={styles.purchaseButton}>Purchase</button>
+                </div>
+            </div>
         </div>
-        <div className={styles.itemDescription}>
-          <p>{item.name}</p>
-          <p>{item.cost} 
-            <img src="/images/donacoin.png" alt="Dona Coin" width={15} height={15}/>
-          </p>
-          <button onClick={getItem} className={styles.purchaseButton}>Purchase</button>
-        </div>
-    </div>
-  );
+    );
 };
