c44d2e1f32aaabecfa85c2142a7baa9445e9c0a1
FRANCISCO-MSI\Francisco MSI <ricfrancvi@gmail.com>
2024-12-19 11:45:07 +0000

    feat status changed on ciew campaign

--- src/services/hubs/events/NewCampaignStatus.ts
+++ src/services/hubs/events/NewCampaignStatus.ts
@@ -5,6 +5,7 @@ import { CampaignStatus } from "@/models/types/CampaignStatus";
 type Data =
 {
     campingId:number,
+    newStatusNumber:CampaignStatus,
     newStatus:string 
 }
 
@@ -18,6 +19,7 @@ export class NewCampaignStatus implements IHubEvent<Data>
         this.name = NewCampaignStatus.name
         this.data = {
             campingId:campaign.id!,
+            newStatusNumber:campaign.status,
             newStatus:CampaignStatus[campaign.status]
         };
     }
